a506f1b9fa0dfbfb993793ad8872da044361459b
codelusion <ash@aisaac.io>
2017-04-06 18:03:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var restify = require('restify');
+var os = require('os');
+var serviceName = 'fakems';
+var serverName = serviceName + '-pid-' + process.pid;
+var hitCount = 0;
+var PORT = process.env.FAKEMS_PORT || 3000;
+
+function add(req, res, next) {
+    hitCount++;
+    res.send(200, getStatus(req));
+    next();
+}
+
+function sub(req, res, next) {
+    hitCount = hitCount > 1 ? hitCount -1 : 0;
+    res.send(200, getStatus(req));
+    next();
+}
+
+function zero(req, res, next) {
+    hitCount = 0;
+    res.send(200, getStatus(req));
+    next();
+}
+
+
+function health(req, res, next) {
+    res.send(200, getStatus(req));
+    next();
+}
+
+function getStatus(req) {
+    return {
+        'route': req.path,
+        'hits' : hitCount,
+        'name': serverName,
+        'host': os.hostname()
+    }
+}
+
+var server = restify.createServer({
+    'name': serverName
+});
+
+
+
+
+server.use(restify.bodyParser({ mapParams: true }));
+
+server.get('/add/', add);
+server.get('/', add);
+server.get('/health/', health);
+server.get('/sub/', sub);
+server.get('/zero/', zero);
+
+server.listen(PORT, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
